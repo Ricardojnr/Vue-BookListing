287a595f0b50f2ab7f9c1b426b35285d6eed3ccb
ricardojnr <44331283+Ricardojnr@users.noreply.github.com>
2020-05-12 00:48:47 -0400

    c

---
+++ src/components/BookItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <li>{{ book.title }} : {{ book.author }}</li>
+</template>
+<script>
+export default {
+  name: "BookItem",
+
+  data() {
+    props["book"];
+    return {
+      key: value,
+    };
+  },
+};
+</script>
+
+<style>
+li {
+  display: block;
+  margin: 0 10px;
+}
+</style>
